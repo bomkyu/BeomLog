fab865d061d30d231f5b30c260e8819932a3cd1c
bomkyu <a01090762806@gmail.com>
2026-03-14 00:00:29 +0900

    chore : font, bg 변경

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
+import Header from './component/Header';
 
-const geistSans = Geist({
+const inter = Inter({
   variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
   subsets: ['latin'],
 });
 
@@ -24,8 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={`${inter.variable} bg-[var(--background)]`}>
+        <Header />
+        <div className='max-w-[1280px] m-auto'>{children}</div>
       </body>
     </html>
   );
