155844d82288f44812ce57026b01a6d2af9792dc
bomkyu <a01090762806@gmail.com>
2026-03-30 19:36:14 +0900

    fix : 컴포넌트 Props 받기로 수정

--- client/src/app/component/Card/PortfolioCard.tsx
+++ client/src/app/component/Card/PortfolioCard.tsx
@@ -1,22 +1,35 @@
 import Link from 'next/link';
 import Typography from '../Typography';
 import Badge from '../Badge';
+import Image, { StaticImageData } from 'next/image';
 
-const PortfolioCard = () => {
+type PortfolioCardProps = {
+  title: string;
+  tag: string;
+  description: string;
+  src: string;
+  img: string | StaticImageData;
+};
+const PortfolioCard = ({
+  title,
+  description,
+  tag,
+  src,
+  img,
+}: PortfolioCardProps) => {
   return (
-    <div className='max-w-[534px] w-full border border-stroke-gray bg-white rounded-2xl'>
-      <Link href={'/'}>
-        <div></div>
+    <div className='max-w-[534px] w-full border border-stroke-gray bg-white rounded-2xl overflow-hidden'>
+      <Link href={src}>
+        <div>
+          <Image src={img} alt='더미이미지' />
+        </div>
         <div className='p-6'>
           <div className='mb-4 self-start'>
-            <Badge color='primary-blue'>React</Badge>
+            <Badge color='primary-blue'>{tag}</Badge>
           </div>
           <div className='flex flex-col gap-4'>
-            <Typography variant='h3'>차세대 커머스 대시보드</Typography>
-            <Typography>
-              실시간 주문 트래킹과 데이터 시각화를 제공하는 B2B 관리자 대시
-              보드를 구축했습니다. 전반적인 성능을 40% 개선했습니다.
-            </Typography>
+            <Typography variant='h3'>{title}</Typography>
+            <Typography>{description}</Typography>
             <div className='flex gap-2'>
               <Typography as='span'>상세보기</Typography>
             </div>
