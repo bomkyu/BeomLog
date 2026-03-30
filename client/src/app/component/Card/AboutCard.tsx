8ed6d0d9bef40e497d67713f39b601a55c8d4337
bomkyu <a01090762806@gmail.com>
2026-03-30 19:35:36 +0900

    chore : 소개 컴포넌트 아이콘 추가

--- client/src/app/component/Card/AboutCard.tsx
+++ client/src/app/component/Card/AboutCard.tsx
@@ -1,15 +1,22 @@
-import { ReactNode } from 'react';
 import Typography from '../Typography';
+import Image from 'next/image';
+import { MAIN_ICON_MAP } from '../icons';
 
 type AboutCardProps = {
-  icon?: ReactNode;
+  icon: keyof typeof MAIN_ICON_MAP;
   title: string;
   description: string;
 };
 const AboutCard = ({ icon, title, description }: AboutCardProps) => {
   return (
-    <div className='p-6 bg-white border border-1 border-[#F1F5F9] rounded-xl '>
-      <div className='mb-6'>{icon}</div>
+    <div className='p-6 bg-white border border-[#F1F5F9] rounded-xl '>
+      <div className='mb-6'>
+        <Image
+          src={MAIN_ICON_MAP[icon]}
+          alt={`${title} icon`}
+          priority={true}
+        />
+      </div>
       <div className='flex flex-col gap-2'>
         <Typography variant='h3'>{title}</Typography>
         <Typography variant='caption'>{description}</Typography>
