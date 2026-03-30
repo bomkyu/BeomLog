7b65d8ee432026989399defcbacef81bdbe77489
bomkyu <a01090762806@gmail.com>
2026-03-30 19:33:13 +0900

    add : 아이콘 추가 및 svg 매핑

---
+++ client/src/app/component/icons/index.tsx
@@ -0,0 +1,38 @@
+import ReactIcon from './react-icon.svg';
+import TSIcon from './ts-icon.svg';
+import NextIcon from './next-icon.svg';
+import TailwindIcon from './tailwin-icon.svg';
+import ReduxIcon from './redux-icon.svg';
+import NestJSIcon from './nest-icon.svg';
+import MysqlIcon from './mysql-icon.svg';
+import PHPIcon from './php-icon.svg';
+import DockerIcon from './docker-icon.svg';
+import FigmaIcon from './figma-icon.svg';
+import NotionIcon from './notion-icon.svg';
+import Introduce from './introduce-icon.svg';
+import Introduce2 from './introduce-icon2.svg';
+import WhiteArrow from './white-arrow-right-icon.svg';
+import MialIcon from './mail-icon.svg';
+import LocationIcon from './location-icon.svg';
+
+export const STACK_ICON_MAP: { [key: string]: string } = {
+  'react-icon': ReactIcon,
+  'ts-icon': TSIcon,
+  'next-icon': NextIcon,
+  'tailwind-icon': TailwindIcon,
+  'redux-icon': ReduxIcon,
+  'nest-icon': NestJSIcon,
+  'mysql-icon': MysqlIcon,
+  'php-icon': PHPIcon,
+  'docker-icon': DockerIcon,
+  'figma-icon': FigmaIcon,
+  'notion-icon': NotionIcon,
+};
+
+export const MAIN_ICON_MAP: { [key: string]: string } = {
+  'introduce-icon': Introduce,
+  'introduce2-icon': Introduce2,
+  'white-arrow-icon': WhiteArrow,
+  'mail-icon': MialIcon,
+  'location-icon': LocationIcon,
+};
