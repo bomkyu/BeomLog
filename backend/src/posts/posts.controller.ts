f6c190879c0e60465bc802d999e5b391b503c496
bomkyu <a01090762806@gmail.com>
2026-04-07 16:50:07 +0900

    add : Post의 controller생성  Swagger등록및 글 작성, 조회 작업

---
+++ backend/src/posts/posts.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+
+@ApiTags('posts') // Swagger에서 'posts' 그룹화
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  @ApiOperation({ summary: '게시글 작성' })
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: '게시글 목록 조회' })
+  findAll() {
+    return this.postsService.findAll();
+  }
+}
