f63d306aa50e1d463e3e903eed0f332c5544a96d
bomkyu <a01090762806@gmail.com>
2026-04-08 16:03:40 +0900

    fix : header Link 연결

--- client/src/app/component/Header.tsx
+++ client/src/app/component/Header.tsx
@@ -2,7 +2,7 @@ import Typography from './Typography';
 
 const Header = () => {
   return (
-    <div className='relative h-[64px] px-16 border-b-1 border-[#E2E8F0] bg-white'>
+    <div className='fixed w-full h-[64px] px-16 border-b-1 border-[#E2E8F0] bg-white'>
       <div className='w-full h-full px-6'>
         <Typography variant='h3' className='flex h-full items-center'>
           Beom&apos;s portfolio
@@ -10,10 +10,18 @@ const Header = () => {
 
         <ul className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
           <li className='flex gap-8'>
-            <Typography variant='body'>소개</Typography>
-            <Typography variant='body'>기술 스택</Typography>
-            <Typography variant='body'>프로젝트</Typography>
-            <Typography variant='body'>연락처</Typography>
+            <a href='#introduce'>
+              <Typography variant='body'>소개</Typography>
+            </a>
+            <a href='#stack'>
+              <Typography variant='body'>기술 스택</Typography>
+            </a>
+            <a href='#project'>
+              <Typography variant='body'>프로젝트</Typography>
+            </a>
+            <a href='#phone'>
+              <Typography variant='body'>연락처</Typography>
+            </a>
           </li>
         </ul>
       </div>
