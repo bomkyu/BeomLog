3c98c97d75d73bd79c1e4066ffc391cbff0905e1
bomkyu <a01090762806@gmail.com>
2026-04-08 16:39:30 +0900

    add : blog Header 작업

---
+++ client/src/app/(blog)/components/Header.tsx
@@ -0,0 +1,14 @@
+import Typography from '@/app/component/Typography';
+
+const Header = () => {
+  return (
+    <div className='fixed w-full h-[64px] px-20 top-0 border-b-1 border-[#E2E8F0] bg-white'>
+      <div className='w-full h-full'>
+        <Typography variant='h3' className='flex h-full items-center'>
+          Beom&apos;sLog
+        </Typography>
+      </div>
+    </div>
+  );
+};
+export default Header;
