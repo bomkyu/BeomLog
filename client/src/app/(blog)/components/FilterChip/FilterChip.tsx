2d767a3ee9ebc68a378b18160b7391ad3fab1243
bomkyu <a01090762806@gmail.com>
2026-04-16 17:44:17 +0900

    add : blog List Page FilterChip 컴포넌트 추가

---
+++ client/src/app/(blog)/components/FilterChip/FilterChip.tsx
@@ -0,0 +1,13 @@
+import Typography from '@/app/component/Typography';
+
+type FilterChipProps = {
+  text: string;
+};
+const FilterChip = () => {
+  return (
+    <div className='inline-block bg-primary-blue rounded-full px-5 py-2'>
+      <Typography className='text-white'>asd</Typography>
+    </div>
+  );
+};
+export default FilterChip;
