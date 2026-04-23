07b9ed570a46cef894b3f0869ae77605de7094ed
bomkyu <a01090762806@gmail.com>
2026-04-24 01:30:40 +0900

    add : handleCreatePosts api 파일 분리

---
+++ client/src/app/(blog)/blog/write/action/createPost.ts
@@ -0,0 +1,45 @@
+const handleCreatePosts = async (formData: FormData) => {
+  const postData = {
+    title: formData.get('title') as string,
+    summary: ((formData.get('title') as string) || '').substring(0, 100),
+    content: formData.get('content') as string,
+    thumbnail: (formData.get('thumbnail') as string) || '',
+    categoryId: Number(formData.get('category')),
+    tags:
+      formData
+        .get('tags')
+        ?.toString()
+        .split(',')
+        .map((t) => t.trim())
+        .filter(Boolean) || [],
+  };
+
+  // console.log('전송 시도 데이터:', postData);
+
+  try {
+    const response = await fetch('http://localhost:4000/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log('DB 저장 성공:', result);
+      alert('게시글이 성공적으로 등록되었습니다! 🚀');
+    } else {
+      const errorData = await response.json();
+      console.error('저장 실패:', errorData);
+      alert(`실패: ${errorData.message || '알 수 없는 에러'}`);
+    }
+  } catch (error) {
+    console.error('네트워크 에러:', error);
+    alert(
+      '서버와 연결할 수 없습니다. 백엔드(4000번)가 켜져 있는지 확인하세요!'
+    );
+  }
+};
+
+export default handleCreatePosts;
