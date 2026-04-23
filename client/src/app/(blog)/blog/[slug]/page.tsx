8c165000aa8996510709257f71613006482b64b0
bomkyu <a01090762806@gmail.com>
2026-04-24 01:26:01 +0900

    add : view page 데이터 연동

--- client/src/app/(blog)/blog/[slug]/page.tsx
+++ client/src/app/(blog)/blog/[slug]/page.tsx
@@ -3,9 +3,47 @@ import Typography from '@/app/component/Typography';
 import CustoMTextArea from '../../components/Comment/CustomTextArea';
 import Buttons from '@/app/component/Buttons';
 
-const BlogViewPage = () => {
+const getPost = async (slug: string) => {
+  const res = await fetch(`http://backend:4000/posts/${slug}`, {
+    next: { revalidate: 60 },
+  });
+
+  console.log(res);
+  if (!res.ok) return null;
+  return res.json();
+};
+
+const BlogViewPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const resolvedParams = await params;
+  const slug = resolvedParams.slug;
+
+  const post = await getPost(slug);
+
+  if (!post) {
+    return (
+      <div className='p-20 text-center'>
+        <Typography variant='h2'>존재하지 않는 게시글입니다. 🥲</Typography>
+      </div>
+    );
+  }
+
+  const date = new Date(post.createdAt);
+
+  const formattedDate = date
+    .toLocaleDateString('ko-KR', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    })
+    .replace(/\s/g, '')
+    .replace(/\.$/, '');
+
   return (
-    <div className='max-w-[1280px] w-full m-auto pt-[60px] bg-white'>
+    <div className='max-w-[1280px] w-full m-auto bg-white'>
       <div className='p-4'>
         <div className='flex relative w-full h-100 rounded-xl overflow-hidden'>
           <img
@@ -14,10 +52,10 @@ const BlogViewPage = () => {
               'https://mblogthumb-phinf.pstatic.net/MjAxNzExMDJfMjcw/MDAxNTA5NjAwODE2MTQy.Oj3_gbsZJyiVChB95iRgSLXEkAEijQgSUIeV70CM_nog.Lt52w9uwt8JGnHPDJTMH2-BZ05mOKIOufg62tHF6kOkg.PNG.brightly29/2017-11-02_14%3B32%3B32.PNG?type=w800'
             }
           />
-          <div className='absolute w-full bottom-0 left-0 p-10'>
-            <Badge color={'primary-blue'}>Engineering</Badge>
+          <div className='absolute flex flex-col gap-2 w-full bottom-0 left-0 p-10'>
+            <Badge color={'blog'}>{post.category.name}</Badge>
             <Typography variant='h1' className='text-white'>
-              모던 웹사이트를 위한 프레임워크 서비스
+              {post.title}
             </Typography>
             <div className='absolute w-full h-full inset-0 bg-linear-[to_top,rgba(0,0,0,0.7)_0%,rgba(0,0,0,0)_50%]' />
           </div>
@@ -27,12 +65,15 @@ const BlogViewPage = () => {
         <div>
           <Typography className='text-lg'>서범규</Typography>
           <Typography variant='caption'>
-            Frontend Devloper &#183; 2025.4.16
+            Frontend Devloper &#183; {formattedDate}
           </Typography>
         </div>
       </div>
       <section className='min-h-[1000px] pt-8 px-10 border-t border-b border-[#F1F5F9]'>
-        Content Area
+        <div
+          className='prose max-w-none'
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
       </section>
       <section className='p-10 bg-[#F8FAFC]'>
         <Typography variant='h3'>
