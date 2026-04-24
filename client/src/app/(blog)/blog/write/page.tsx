e42916c2523f622a2d555d8bf1b73fbd82b4e55f
bomkyu <a01090762806@gmail.com>
2026-04-25 01:18:11 +0900

    add : custom selectBox추가 및 디자인 Input으로 적용

--- client/src/app/(blog)/blog/write/page.tsx
+++ client/src/app/(blog)/blog/write/page.tsx
@@ -1,17 +1,66 @@
 'use client';
-
-import ConTactInput from '@/app/component/InputField/ConTactInput';
 import Typography from '@/app/component/Typography';
 import Form from 'next/form';
 import ImageUploadDropzone from '../../components/UploadDropZone';
 import TiptapEditor from '../../components/Editor/TiptapEditor';
 import Buttons from '@/app/component/Buttons';
-import { useState } from 'react';
-import handleCreatePosts from './action/createPost';
+import { useEffect, useState } from 'react';
+import {
+  getCategoriesFromApi,
+  handleCreatePosts,
+  uploadImageApi,
+} from '@/app/lib/api';
+import SelectBox from '../../components/SelectBox/SelectBox';
+import Input from '../../components/Input/Input';
+import { useRouter } from 'next/navigation';
 
 const Write = () => {
   const [content, setContent] = useState('');
   const [thumbnailUrl, setThumbnailUrl] = useState('');
+  const [isUploading, setIsUploading] = useState(false);
+  const [categories, setCategories] = useState<{ id: number; name: string }[]>(
+    []
+  );
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(0);
+  const router = useRouter();
+
+  const handleImageUpload = async (file: File | null) => {
+    if (!file) {
+      setThumbnailUrl('');
+      return;
+    }
+
+    try {
+      setIsUploading(true);
+
+      const { url } = await uploadImageApi(file);
+
+      setThumbnailUrl(url);
+      console.log('이미지 업로드 성공:', url);
+    } catch (error) {
+      const message =
+        error instanceof Error ? error.message : '알 수 없는 오류 발생';
+      alert(message);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  // 1. 카테고리 목록 불러오기
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const data = await getCategoriesFromApi();
+      setCategories(data);
+    };
+    fetchCategories();
+  }, []);
+
+  const onSubmit = async (formData: FormData) => {
+    await handleCreatePosts(formData, {
+      content: content,
+      thumbnail: thumbnailUrl,
+    });
+  };
 
   return (
     <div className='py-8'>
@@ -22,36 +71,39 @@ const Write = () => {
         </Typography>
       </div>
       <section className='p-8 rounded-xl bg-white'>
-        <Form action={handleCreatePosts}>
+        <Form action={onSubmit}>
           <div className='flex flex-col gap-6'>
-            <ConTactInput
-              name='title'
-              label='제목'
-              placeholder='제목을 입력하세요'
-            />
+            <Input name='title' label='제목' placeholder='제목을 입력하세요' />
             <div className='flex gap-6 w-full'>
-              <ConTactInput
-                name='category'
+              <SelectBox
                 label='카테고리'
-                placeholder='카테고리 선택'
+                options={categories}
+                value={selectedCategoryId}
+                onChange={(id) => setSelectedCategoryId(id)}
+                placeholder='카테고리를 선택하세요'
               />
-              <ConTactInput
+              <input type='hidden' name='category' value={selectedCategoryId} />
+
+              <Input
                 name='tags'
                 label='태그 (쉼표로 구분)'
                 placeholder='예: 개발, 디자인, 일상'
               />
             </div>
             <div className='flex flex-col gap-2'>
-              <Typography className='text-xs font-medium text-slate-700 ml-1'>
+              <Typography className='text-sm font-medium text-slate-700 ml-1'>
                 대표 이미지 (썸네일)
               </Typography>
-              <ImageUploadDropzone
-              // onUploadSuccess={(url) => setThumbnailUrl(url)}
-              />
+              <ImageUploadDropzone onFileSelect={handleImageUpload} />
               <input type='hidden' name='thumbnail' value={thumbnailUrl} />
+              {isUploading && (
+                <p className='text-xs text-blue-500 ml-1'>
+                  이미지를 업로드 중입니다...
+                </p>
+              )}
             </div>
             <div className='flex flex-col gap-2'>
-              <Typography className='text-xs font-medium text-slate-700 ml-1'>
+              <Typography className='text-sm font-medium text-slate-700 ml-1'>
                 본문 내용
               </Typography>
               <TiptapEditor
@@ -62,8 +114,24 @@ const Write = () => {
             </div>
           </div>
           <div className='flex justify-end gap-3 mt-6 pt-4 border-t border-[#F1F5F9]'>
-            <Buttons btnType={'outLine'}>임시저장</Buttons>
-            <Buttons btnType={'primary'}>저장하기</Buttons>
+            <Buttons
+              btnType='outLine'
+              type='button'
+              onClick={() => {
+                if (
+                  confirm(
+                    '작성 중인 내용은 저장되지 않습니다. 취소하시겠습니까?'
+                  )
+                ) {
+                  router.back();
+                }
+              }}
+            >
+              취소
+            </Buttons>
+            <Buttons btnType='primary' disabled={isUploading}>
+              {isUploading ? '업로드 중...' : '저장하기'}
+            </Buttons>
           </div>
         </Form>
       </section>
