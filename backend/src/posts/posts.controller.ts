fecd274f6464992ae5bc57491703fea33069aeb0
bomkyu <a01090762806@gmail.com>
2026-04-24 23:15:25 +0900

    add : 파일 업로드 기능을 위해 이미지 저장 인터페이스 추가

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -6,10 +6,15 @@ import {
   ParseIntPipe,
   Post,
   Query,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @ApiTags('posts') // Swagger에서 'posts' 그룹화
 @Controller('posts')
@@ -37,5 +42,25 @@ export class PostsController {
       page: Number(page),
       category: category === 'all' ? undefined : category,
     });
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads', // 파일이 저장될 실제 폴더 경로
+        filename: (req, file, callback) => {
+          // 파일명이 겹치지 않게 타임스탬프 + 랜덤값으로 생성
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          callback(null, `${uniqueSuffix}${ext}`);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const imageUrl = `http://localhost:4000/uploads/${file.filename}`;
+    return { url: imageUrl };
   }
 }
