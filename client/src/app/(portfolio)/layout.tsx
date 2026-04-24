73f33365104d2dcd52ef67cb7c756a38c484c899
bomkyu <a01090762806@gmail.com>
2026-04-24 23:23:27 +0900

    fix : body html이 중첩되었던 문제 해결

--- client/src/app/(portfolio)/layout.tsx
+++ client/src/app/(portfolio)/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+
 import '../globals.css';
 import Header from '../component/Header';
 import Footer from '../component/Footer';
-
-const inter = Inter({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: `Beom's portfolio`,
@@ -20,12 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.variable} bg-background`}>
-        <Header />
-        <div className='max-w-[1280px] m-auto'>{children}</div>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Header />
+      <main className='max-w-[1280px] m-auto'>{children}</main>
+      <Footer />
+    </>
   );
 }
