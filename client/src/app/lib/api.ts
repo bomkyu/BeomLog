6353d56f6df856cdda9221af8c333536f41ce778
bomkyu <a01090762806@gmail.com>
2026-04-25 01:23:25 +0900

    feat: API 모듈화 및 환경별 BASE_URL 적용
    - getPosts, getCategories 등 API 호출 로직 통합
    
    - SSR과 클라이언트 환경에 따른 BASE_URL 분기 처리
    
    - 이미지 업로드 및 게시글 생성 핸들러 최적화

--- client/src/app/lib/api.ts
+++ client/src/app/lib/api.ts
@@ -1,6 +1,7 @@
-import 'server-only';
-
-const BASE_URL = process.env.BACKEND_INTERNAL_URL;
+export const BASE_URL =
+  typeof window === 'undefined'
+    ? process.env.BACKEND_INTERNAL_URL
+    : process.env.NEXT_PUBLIC_BACKEND_URL;
 
 /** * 게시글 목록 가져오기 (필터 및 페이징 적용) */
 export const getPostsFromApi = async (
@@ -48,4 +49,73 @@ export const getPost = async (slug: string) => {
   console.log(res);
   if (!res.ok) return null;
   return res.json();
+};
+
+/** write페이지 데이테 보내기 */
+export const handleCreatePosts = async (
+  formData: FormData,
+  extraData: { content: string; thumbnail: string }
+) => {
+  const plainTextContent = (formData.get('content') as string) || '';
+
+  const postData = {
+    title: formData.get('title') as string,
+    summary: plainTextContent.replace(/<[^>]*>/g, '').substring(0, 150),
+    content: extraData.content,
+    thumbnail: extraData.thumbnail,
+    categoryId: Number(formData.get('category')),
+    tags:
+      formData
+        .get('tags')
+        ?.toString()
+        .split(',')
+        .map((t) => t.trim())
+        .filter(Boolean) || [],
+  };
+
+  try {
+    const response = await fetch('http://localhost:4000/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log('DB 저장 성공:', result);
+      alert('게시글이 성공적으로 등록되었습니다! 🚀');
+    } else {
+      const errorData = await response.json();
+      console.error('저장 실패:', errorData);
+      alert(`실패: ${errorData.message || '알 수 없는 에러'}`);
+    }
+  } catch (error) {
+    console.error('네트워크 에러:', error);
+    alert(
+      '서버와 연결할 수 없습니다. 백엔드(4000번)가 켜져 있는지 확인하세요!'
+    );
+  }
+};
+
+export type UploadResponse = {
+  url: string;
+};
+
+export const uploadImageApi = async (file: File): Promise<UploadResponse> => {
+  const formData = new FormData();
+  formData.append('file', file);
+
+  const response = await fetch(`${BASE_URL}/posts/upload`, {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => ({}));
+    throw new Error(errorData.message || '이미지 업로드에 실패했습니다.');
+  }
+
+  return response.json();
 };
