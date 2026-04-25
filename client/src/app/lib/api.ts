a91fcae7f18d3955c0cd866b3264e589c17a6b0c
bomkyu <a01090762806@gmail.com>
2026-04-25 17:30:44 +0900

    feat : 업로드 status를 blog/write 페이지에 boolean값을 전달해서 성공 여부에 따라 route할 수 있게 코드 수정

--- client/src/app/lib/api.ts
+++ client/src/app/lib/api.ts
@@ -74,7 +74,7 @@ export const handleCreatePosts = async (
   };
 
   try {
-    const response = await fetch('http://localhost:4000/posts', {
+    const response = await fetch(`${BASE_URL}/posts`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -83,19 +83,18 @@ export const handleCreatePosts = async (
     });
 
     if (response.ok) {
-      const result = await response.json();
-      console.log('DB 저장 성공:', result);
-      alert('게시글이 성공적으로 등록되었습니다! 🚀');
+      // const result = await response.json();
+      // console.log('DB 저장 성공:', result);
+      // alert('게시글이 성공적으로 등록되었습니다! 🚀');
+      return true;
     } else {
       const errorData = await response.json();
-      console.error('저장 실패:', errorData);
+      // console.error('저장 실패:', errorData);
       alert(`실패: ${errorData.message || '알 수 없는 에러'}`);
     }
   } catch (error) {
     console.error('네트워크 에러:', error);
-    alert(
-      '서버와 연결할 수 없습니다. 백엔드(4000번)가 켜져 있는지 확인하세요!'
-    );
+    alert('서버와 연결할 수 없습니다.');
   }
 };
 
