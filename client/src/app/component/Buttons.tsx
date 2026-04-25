bbf8eb9039bd0d4c4250a0495a5f61b3c76307b6
bomkyu <a01090762806@gmail.com>
2026-04-25 19:47:59 +0900

    feat : postAction 버튼 스타일 추가

--- client/src/app/component/Buttons.tsx
+++ client/src/app/component/Buttons.tsx
@@ -2,16 +2,17 @@ import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 type ButtonsProps = ComponentPropsWithoutRef<'button'> & {
   children: ReactNode;
-  btnType: 'primary' | 'secondary' | 'outLine';
+  btnType: 'primary' | 'secondary' | 'outLine' | 'postAction';
 };
 
 const baseStyles =
-  'px-6 py-2 rounded-lg font-semibold transition-all duration-200 cursor-pointer';
+  ' rounded-lg font-semibold transition-all duration-200 cursor-pointer';
 
 const typeStylesMap = {
-  primary: 'bg-primary-blue text-white ',
-  secondary: 'bg-primary-blue text-blue-600',
-  outLine: 'bg-transparent border border-outline-gray text-gray-800',
+  primary: 'px-6 py-2 bg-primary-blue text-white ',
+  secondary: 'px-6 py-2 bg-primary-blue text-primary-blue',
+  outLine: 'px-6 py-2 bg-transparent border border-outline-gray text-gray-800',
+  postAction: 'px-4 py-0 gap-2 h-10',
 };
 
 const Buttons = ({ children, btnType, className, ...props }: ButtonsProps) => {
