f0c6f2a78e30d0c94546d66cbda88b0b5273ff5f
bomkyu <a01090762806@gmail.com>
2026-04-25 19:45:40 +0900

    add : client 컴포넌트 분리

---
+++ client/src/app/(blog)/blog/[slug]/components/DeleteBtnLayout.tsx
@@ -0,0 +1,26 @@
+'use client';
+import Buttons from '@/app/component/Buttons';
+import Typography from '@/app/component/Typography';
+import { Trash2 } from 'lucide-react';
+
+type DeleteBtnLayoutProps = {
+  id: number;
+};
+
+const handleDelete = async (id: number) => {
+  if (confirm(`정말 삭제할까요?`)) {
+  }
+};
+const DeleteBtnLayout = ({ id }: DeleteBtnLayoutProps) => {
+  return (
+    <Buttons
+      btnType='PostAction'
+      className='bg-[#FEF2F2]'
+      onClick={() => handleDelete(id)}
+    >
+      <Trash2 size={14} color='#DC2626'></Trash2>
+      <Typography className='text-sm text-[#DC2626]'>삭제</Typography>
+    </Buttons>
+  );
+};
+export default DeleteBtnLayout;
