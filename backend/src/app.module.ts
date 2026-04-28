0ac0727e91ce059168e7ab721461de357afe58d7
bomkyu <a01090762806@gmail.com>
2026-04-28 17:58:03 +0900

    feat : ImagesModule추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,6 +10,7 @@ import { Comment } from './comments/entities/comment.entity';
 import { PostsModule } from './posts/posts.module';
 import { CategoriesModule } from './categories/categories.module';
 import { TagsModule } from './tags/tags.module';
+import { ImagesModule } from './images/images.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { TagsModule } from './tags/tags.module';
     PostsModule,
     CategoriesModule,
     TagsModule,
+    ImagesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
