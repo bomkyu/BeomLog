f5f2ec004d6ed57a7688c0b469ac8586e803d3d0
bomkyu <a01090762806@gmail.com>
2026-04-30 18:48:42 +0900

    feat: PostImage와 Post 엔티티 간의 외래키 관계 명시 및 설정 최적화
    - @JoinColumn 데코레이터를 사용하여 postsId를 명시적인 외래키 컬럼으로 지정
    
    - @ManyToOne 관계에 onDelete: 'CASCADE' 옵션을 적용하여 게시글 삭제 시 관련 이미지 자동 삭제 처리
    
    - 엔티티 구조와 물리 DB 컬럼 간의 일관성 확보를 위한 스키마 정의 수정

--- backend/src/images/entities/image.entity.ts
+++ backend/src/images/entities/image.entity.ts
@@ -1,5 +1,11 @@
 import { Post } from 'src/posts/entities/post.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('images')
 export class PostImage {
@@ -9,7 +15,9 @@ export class PostImage {
   @Column()
   url: string;
 
+  // 관계 정의와 실제 DB 컬럼명을 JoinColumn으로 묶어줌
   @ManyToOne(() => Post, (post) => post.images, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'postsId' }) // DB의 postsId 컬럼을 이 관계의 외래키로 쓰겠다고 명시
   post: Post;
 
   @Column()
