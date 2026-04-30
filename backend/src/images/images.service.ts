f19674de9e8ef365201b3a5f63e572693a999531
bomkyu <a01090762806@gmail.com>
2026-04-30 18:46:43 +0900

    feat: 실제 이미지 파일 삭제 메서드(removeImageFile) 추가
    - url에서 파일명을 추출하여 uploads 폴더 내 물리적 파일 삭제 로직 구현
    
    - fs.access 및 fs.unlink를 사용하여 파일 존재 확인 후 안전하게 삭제 처리
    
    - 이미지 삭제 시 발생하는 예외 상황에 대한 에러 핸들링 추가

--- backend/src/images/images.service.ts
+++ backend/src/images/images.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { PostImage } from './entities/image.entity';
 import { Repository } from 'typeorm';
 import { Post } from 'src/posts/entities/post.entity';
+import { promises as fs } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class ImagesService {
@@ -25,5 +27,20 @@ export class ImagesService {
       });
     });
     return await this.imagesRepository.save(images);
+  }
+
+  async removeImageFile(url: string) {
+    try {
+      const fileName = url.split('/').pop();
+      if (!fileName) return;
+
+      const filePath = join(process.cwd(), 'uploads', fileName);
+
+      await fs.access(filePath); // 파일 있는지 확인
+      await fs.unlink(filePath); // 파일 삭제
+      console.log(`파일 삭제 성공: ${filePath}`);
+    } catch (error) {
+      console.log('ERROR : ', error);
+    }
   }
 }
