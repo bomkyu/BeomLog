a2210060f977ac3e8d2cdfb7bb9dd85369999c47
bomkyu <a01090762806@gmail.com>
2026-04-30 19:09:54 +0900

    feat : 정규화된 images 배열에서 isThumbnail 플래그를 기반으로 대표 이미지 탐색 로직 추가

--- client/src/app/(blog)/blog/[slug]/page.tsx
+++ client/src/app/(blog)/blog/[slug]/page.tsx
@@ -27,12 +27,17 @@ const BlogViewPage = async ({
     );
   }
 
+  const thumbnailImage =
+    post.images?.find(
+      (img: { isThumbnail: boolean }) => img.isThumbnail === true
+    ) || post.images?.[0];
+
   return (
     <div className='max-w-[1280px] w-full m-auto bg-white'>
       <div className='p-4'>
         <div className='flex relative w-full h-100 rounded-xl overflow-hidden'>
           <Image
-            src={post.thumbnail}
+            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${thumbnailImage.url}`}
             alt={`${post.title}이미지`}
             fill
             unoptimized
