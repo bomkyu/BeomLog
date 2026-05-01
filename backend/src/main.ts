d9f602c24575727a33e27afb55977f5f6dd51d9d
bomkyu <a01090762806@gmail.com>
2026-05-01 19:47:09 +0900

    feat: CORS 및 세션 설정 배포 준비
    - NEXT_PUBLIC_FRONTEND_URL 환경 변수 적용
    - 세션 쿠키 httpOnly 설정 및 credentials 허용 추가
    - ConfigService를 통한 세션 시크릿 관리

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,39 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+import session from 'express-session';
 
 async function bootstrap() {
   // <NestExpressApplication> 제네릭 추가
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      `${process.env.NEXT_PUBLIC_FRONTEND_URL}`,
+      // 도메인 주소
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  // 1. 세션 미들웨어 설정
+  app.use(
+    session({
+      secret:
+        configService.get<string>('SESSION_SECRET') ||
+        'temporary-dev-secret-key',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        // 배포시 true로 변경해야함
+        secure: false,
+        maxAge: 3600000, // 1시간 (밀리초 단위)
+      },
+    }),
+  );
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
