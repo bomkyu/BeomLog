93932bffb32374ae58b12d176b92d4263365bbe6
bomkyu <a01090762806@gmail.com>
2026-05-02 00:25:48 +0900

    feat(layout): RootLayout에 AuthProvider 적용
    - 애플리케이션 전역에 인증 상태 주입
    - 서버 컴포넌트 구조를 유지하며 클라이언트 세션 체크 로직 통합

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import AuthProvider from './component/AuthProvider';
 
 const inter = Inter({
   variable: '--font-geist-sans',
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.variable} bg-background`}>
-        <div>{children}</div>
+        <AuthProvider>
+          <div>{children}</div>
+        </AuthProvider>
       </body>
     </html>
   );
