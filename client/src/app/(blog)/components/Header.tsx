c2dc30813cf181e7e7bb4892f73fd05b351a27b8
bomkyu <a01090762806@gmail.com>
2026-05-04 15:38:49 +0900

    chore : blogHeader 링크 추가

--- client/src/app/(blog)/components/Header.tsx
+++ client/src/app/(blog)/components/Header.tsx
@@ -1,12 +1,15 @@
 import Typography from '@/app/component/Typography';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className='fixed w-full h-[64px] px-20 top-0 border-b-1 border-[#E2E8F0] bg-white z-50'>
       <div className='w-full h-full'>
-        <Typography variant='h3' className='flex h-full items-center'>
-          Beom&apos;sLog
-        </Typography>
+        <Link href={'/blog'}>
+          <Typography variant='h3' className='flex h-full items-center'>
+            Beom&apos;sLog
+          </Typography>
+        </Link>
       </div>
     </div>
   );
