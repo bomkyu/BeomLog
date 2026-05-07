0b554daa8567a00045577aae4440364b259166bf
bomkyu <a01090762806@gmail.com>
2026-05-07 20:58:52 +0900

    feat : List 반응형 PC : 3, Tablet : 2, Mobile 1

--- client/src/app/(blog)/components/Card/CardLayout.tsx
+++ client/src/app/(blog)/components/Card/CardLayout.tsx
@@ -9,7 +9,7 @@ const CardLayout = ({ data }: { data: Post[] }) => {
       {data.length === 0 ? (
         <Nodata />
       ) : (
-        <div className='grid grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
           {data.map((item) => (
             <Link key={item.id} href={`/blog/${item.id}`}>
               <Card {...item} />
